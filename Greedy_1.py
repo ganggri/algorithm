2a0e463cf03eac0698ecfaeb87d9b54f1a170ced
ganggri <cgi5909@ajou.ac.kr>
2023-01-02 17:18:20 +0900

    알고리즘 풀이 p99 1.1 시간복잡도 logN

--- Greedy_1.py
+++ Greedy_1.py
@@ -1,17 +1,18 @@
 import time
+
+n, k = map(int,(input().split()))
 start = time.time()
 
-a, b = map(int,(input().split()))
 count = 0
 
-while a != 1:           # a의 값이 1이 된다면 종료
-    if(a % b == 0):     # a를 b로 나눈 나머지가 0이면 b로 나눈 후 a값 할당 후 카운트 +1
-        a = a // b
-        count += 1
-    else:
-        a = a - 1       # 나머지가 0이 아닌 경우 a의 값을 -1을 한 후 카운트 +1
-        count += 1
-print(count)
+while n >= k:                    # n이 k보다 작다면 나눌 수 없기에 탈출
+    check = (n // k) * k
+    count += n - check
+    n = check / k
+    count += 1
+
+count += n - 1
+print(int(count))
 
 end = time.time()
-print(f"{end - start:.5f} sec")
+print(f"{end - start:.2f} sec")
