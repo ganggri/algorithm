0bf0cfebdfd39285e4920284220709ce3c3c9d69
ganggri <cgi5909@ajou.ac.kr>
2023-01-02 23:10:13 +0900

    알고리즘 풀이 p99 추가문제 1.1 정렬이 필요없는 경우 정렬 제거

--- Greedy_2.py
+++ Greedy_2.py
@@ -5,14 +5,14 @@ import time, sys
 
 nums = list(map(int,sys.stdin.readline().rstrip()))
 start = time.time()
-
-nums = sorted(nums,reverse=True)            # 입련된 정수를 내림차순 정렬
+    # 변경내용 : 1이나 0이 없을 경우 정렬 안함.
 
 # 문제에서 두가지의 숫자를 각각 계산하기 때문에 여기서 앞 뒤의 문자를 0이나 1이 아니면 전부 순서대로 곱한 후
 # 나머지 숫자는 더한다. 하지만 내림차순으로 정렬하였기에 0이나 1을 매번 체크하지 않고 리스트를 분할한다.
 total = 1
 
 if 1 in nums or 0 in nums:
+    nums = sorted(nums,reverse=True)            # 입련된 정수를 내림차순 정렬
     location = nums.index(1) if 1 in nums else nums.index(0)
     over = nums[:location]
     under = nums[location:]
