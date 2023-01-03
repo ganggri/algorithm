8af749af3d972ebba2b29d810e7728a18fa36272
ganggri <cgi5909@ajou.ac.kr>
2023-01-03 17:39:39 +0900

    알고리즘 풀이 구현 왕실의 나이트 1.0

---
+++ Implements/Implements_2.py
@@ -0,0 +1,29 @@
+# 왕실의 나이트
+# 8*8의 체스판 위에 나이트가 움직일 수 있는 경우의 수 구하기
+# 문제접근 : 그냥 8가지의 경우의수 순회하며 구하기..?
+# 알파벳 to num은 dict type으로 치환
+
+all = input()
+x = all[0]
+y = int(all[1])
+
+dx = [2,2,-2,-2,1,1,-1,-1]
+dy = [-1,1,-1,1,2,-2,2,-2]
+d = {"a" : 1,
+    "b" : 2,
+    "c" : 3,
+    "d" : 4,
+    "e" : 5,
+    "f" : 6,
+    "g" : 7,
+    "h" : 8}
+
+x = d[x]
+count = 0
+for i in range(8):
+    nx = x + dx[i]
+    ny = y + dy[i]
+    if nx > 8 or nx < 1 or ny > 8 or ny < 1:
+        continue
+    count += 1
+print(count)
